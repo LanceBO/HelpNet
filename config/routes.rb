bd5212b3374e6fadde513079156d0be59fdcdce2
ugo lonobile <lonodert@hotmail.fr>
2022-11-28 16:28:56 +0100

    chatroom

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get "/chatroom", to: "chatrooms#show"
   devise_for :users
   root to: "pages#home"
+  resources :issues do
+    resources :tickets
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :chatrooms, only: :show
 end
