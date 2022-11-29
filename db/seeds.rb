5c15b37eb9cc51ab3453a4c94ad88a7015406cda
Basile Mazon <basile.mazon@gmail.com>
2022-11-29 11:32:50 +0100

    BB-Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all
+Training.destroy_all
+Booking.destroy_all
+Ticket.destroy_all
+Issue.destroy_all
+
+user = User.create(email: "helpnet@gmail.com", password: "123456", first_name: "Xavier", last_name: "Clark")
+
+training = Training.create(title: "Learn how to code", category: "code", description: "Hello World", price: 100)
+
+booking = Booking.create(training: Training.last, user: User.last)
+
+ticket = Ticket.create(status: nil, category: "Email Send", url: "test url", user: User.last)
+
+issues = Issue.create(ticket: Ticket.last, solution: "Test Solution", status: nil, user: User.last)
