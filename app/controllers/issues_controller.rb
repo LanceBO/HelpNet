551f9058dabf44f1187bffdfebd95c263423fb7a
Olivier Bouly <oliv.bouly@gmail.com>
2022-11-30 12:36:42 +0100

    formulaire steps 1 2 3

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -8,19 +8,39 @@ class IssuesController < ApplicationController
   end
 
   def new
+    session[:issue_params] ||= {}
+    @issue = Issue.new(session[:issue_params])
+    @issue.current_step = session[:issue_step]
     @issue = Issue.new
   end
 
   def create
-    @issue = Issue.new(issue_params)
-    @issue.user = current_user
-    @issue.save
+    @issue = Issue.new(params[:issue])
+    @issue.current_step = session[:issue_step]
+    if @issue.valid?
+      if params[:back_button]
+        @issue.previous_step
+      elsif @issue.last_step?
+        @issue.save if @issue.all_valid?
+      else
+        @issue.next_step
+      end
+      session[:issue_step] = @issue.current_step
+    end
+    if @issue.new_record?
+      render 'new'
+    else
+      session[:issue_step] = session[:issue_params] = nil
+      flash[:notice] = "Issue saved."
+      redirect_to @issue
+    end
+    render 'new'
   end
 
   private
 
   def issue_params
-    params.require(:issue).permit(:solution)
+    params.require(:issue).permit(:category, :subcategory, :subsubcategory)
   end
 
 end
