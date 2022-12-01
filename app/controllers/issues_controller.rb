105bdb4a972cc84453e18899b031d24172c18ace
Basile Mazon <basile.mazon@gmail.com>
2022-12-01 10:28:24 +0100

    YESMAN

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -8,39 +8,19 @@ class IssuesController < ApplicationController
   end
 
   def new
-    session[:issue_params] ||= {}
-    @issue = Issue.new(session[:issue_params])
-    @issue.current_step = session[:issue_step]
-    @issue = Issue.new
+   @issue = Issue.new
   end
 
   def create
-    @issue = Issue.new(params[:issue])
-    @issue.current_step = session[:issue_step]
-    if @issue.valid?
-      if params[:back_button]
-        @issue.previous_step
-      elsif @issue.last_step?
-        @issue.save if @issue.all_valid?
-      else
-        @issue.next_step
-      end
-      session[:issue_step] = @issue.current_step
-    end
-    if @issue.new_record?
-      render 'new'
-    else
-      session[:issue_step] = session[:issue_params] = nil
-      flash[:notice] = "Issue saved."
-      redirect_to @issue
-    end
-    render 'new'
+    @issue = Issue.new(issue_params)
+    @issue.user = current_user
+    @issue.save
   end
 
   private
 
   def issue_params
-    params.require(:issue).permit(:category, :subcategory, :subsubcategory)
+    params.require(:issue).permit(:subsubcategory)
   end
 
 end
