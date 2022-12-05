212d6bf4b6fe623b84137949352f97e35742aa63
Olivier Bouly <oliv.bouly@gmail.com>
2022-12-05 12:48:03 +0100

    Rectification de texte et de syntaxe

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -5,7 +5,7 @@ class Issue < ApplicationRecord
 
   CATEGORIES = ["Problème matériel", "Problème sur internet", "Problème e-mail"]
   SUBCATEGORIES = ["Je n'arrive pas à me connecter", "Je n'arrive à envoyer un e-mail", "Je ne reçois plus mes e-mails", "Problème avec une pièce jointe", "Je reçois beaucoup de SPAMS", "Je n'arrive pas à supprimer un e-mail"]
-  SUBSUBCATEGORIES = ["Je n'arrive pas à mettre jointe dans mon e-mail", "Ma pièce jointe jointe ne s'envoie pas", "Erreur : La taille de la pièce jointe dépasse la limite maximale", "Je n'arrive à pas télécharger une pièce jointe d'un e-mail reçu"]
+  SUBSUBCATEGORIES = ["Je n'arrive pas à mettre une pièce jointe dans mon e-mail", "Ma pièce jointe ne s'envoie pas", "Erreur : La taille de la pièce jointe dépasse la limite maximale", "Je n'arrive à pas télécharger une pièce jointe d'un e-mail reçu"]
   validates :subsubcategory, presence: true, inclusion: { in: SUBSUBCATEGORIES}
 
 end
